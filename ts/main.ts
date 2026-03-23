3325897a3e0dbd043308d2283d3e70ff8f772cf3
LawrenceSong06 <lingyis@andrew.cmu.edu>
2026-03-23 04:07:52 -0400

    customizing cursor

--- ts/main.ts
+++ ts/main.ts
@@ -48,16 +48,16 @@ class PivotBox {
 	set_pivot2(x : number, y : number){
 		this.pivot2 = new Point(x, y);
 	}
-	diameter(){
-		return this.pivot1.distance_to(this.pivot2);
+	side_length(){
+		return Math.sin(1/4 * Math.PI) * this.pivot1.distance_to(this.pivot2);
 	}
 	rotation(){
-		return this.pivot1.angle_between(this.pivot2);
+		return this.pivot1.angle_between(this.pivot2) - 45;
 	}
 	render() : void {
-		const size = Math.sin(1/4 * Math.PI) * this.diameter();
+		const size = this.side_length();
 		const position = {x: this.pivot1.x, y: this.pivot1.y};
-		const rotation = this.rotation() - 45;
+		const rotation = this.rotation();
 		this.box.size(size, size);
 		this.box.transform({position: position, rotate: rotation, origin: "top left"})
 	}
@@ -69,19 +69,28 @@ window.addEventListener("load", (e: Event) => {
 	// The third parameter sets the trial engine in "verbose" mode or not -- if it is set to "true", all the events will be logged to the Console. (You may wish to set it to "false" if you find these logs overwhelming.)
 	const trial : Trial = new Trial(tasksLength, "teamName", true);
 	
+	document.getElementById("main").style.alignItems = "flex-start";
+	document.getElementById("main").style.justifyContent = "flex-end";
+
 	// You also need to add some way for the user to indicate they are done with their task. 
 	// Whatever it is, it should call the trial.submitPosition() method.
 	// Within the "trackpad + click" constraints, this can be whatever you want it to be (e.g. a button, some kind of swipe gesture, double-clicking on the square itself...). Consider what you've learned about Fitts's Law. :)
 	// Here is a bare version, just using an HTML button:
+	let appArea = document.getElementById("applicationArea");
+	
+	let toolbar : HTMLDivElement = document.createElement("div");
+	toolbar.style.flex = "1";
+	toolbar.style.width = "100%";
+	appArea.appendChild(toolbar);
+	
 	let submitButton : HTMLButtonElement = document.createElement("button");
-	document.getElementById("applicationArea").appendChild(submitButton);
 	submitButton.innerText = "yes, that looks good";
-
+	toolbar.appendChild(submitButton);
+	
 	submitButton.addEventListener("click", (e: PointerEvent) => {
 		trial.submitPosition();
 	})
 	// =========== /end required =========== 
-
 
 	// ====== Getting the manipulable elements =============
 	// Ask the trial engine for the stuff you can manipulate:
@@ -90,14 +99,49 @@ window.addEventListener("load", (e: Event) => {
 	// applicationElements.svg is the overall svg drawable area you can add things to:
 	// Documentation is here: https://svgjs.dev/docs/3.2/container-elements/#svg-svg
 	let svg : svgdotjs.Svg = applicationElements.svg;
+	svg.node.style.margin = "2em";
+	svg.node.style.marginRight = "0";
 	// The main thing you are likely to want to do with it is adding new elements.
 	// For example, if you wanted to add a new rectangle for a toolbox at the location 5,10 on the screen,
 	// you could call: applicationElements.svg.rect(10, 10).move(5,10);
 
-	
 	// applicationElements.grid is the svg group containing the lines drawn in the background.
 	let grid : svgdotjs.G = applicationElements.grid;
 	// You could add other background-y things to this group. You also may remove the grid lines with grid.clear() (but I can't personally think of a good reason why you would want to?).
+
+	// =========== Custom Cursor ==========
+	let closer_to_pivot1 : boolean = false;
+
+	let offsetX = 0;
+	let offsetY = 0;
+	let cursor_hori : svgdotjs.Line = svg.line([[0, 0], [2*canvasSize, 0]]).fill("none").stroke("#ff0000");
+	let cursor_verti : svgdotjs.Line = svg.line([[0, 0], [0, 2*canvasSize]]).fill("none").stroke("#ff0000");
+	cursor_hori.opacity(0);
+	cursor_verti.opacity(0);
+
+	let follower = svg.polygon("0,0 0,1, 1,0").fill("#8e8e8e").stroke("none");
+	follower.attr('pointer-events', 'none');
+	document.addEventListener("mousemove", (e : MouseEvent) => {
+		if(document.pointerLockElement == box.node) {
+			offsetX = Math.min(Math.max(0, offsetX + e.movementX), canvasSize);
+			offsetY = Math.min(Math.max(0, offsetY + e.movementY), canvasSize);
+			cursor_hori.transform({position:{x: 0, y: offsetY}, origin: "center"});
+			cursor_verti.transform({position:{x: offsetX, y: 0}, origin: "center"});
+
+			if (closer_to_pivot1){
+				pivot_box.set_pivot1(offsetX, offsetY);
+			}else {
+				pivot_box.set_pivot2(offsetX, offsetY);
+			}
+			pivot_box.render();
+
+			const x = pivot_box.pivot1.x
+			const y = pivot_box.pivot1.y
+
+			follower.size(pivot_box.side_length(), pivot_box.side_length());
+			follower.transform({position:{x, y}, rotate: pivot_box.rotation(), origin: "top left"}); // "cx"/"cy" are the x and y positions of the center point of the shape
+		}
+	});
 
 
 	// And applicationElements.box is the box itself. :) You can change it with any of the things at https://svgjs.dev/docs/3.2/manipulating/
@@ -119,8 +163,6 @@ window.addEventListener("load", (e: Event) => {
 	pivot_box.set_pivot1(default_pivot1.x, default_pivot1.y);
 	pivot_box.set_pivot2(default_pivot2.x, default_pivot2.y);
 	pivot_box.render();
-
-	let closer_to_pivot1 : boolean = false;
 
 	// ====== Manipulating them =============
 	// For example, you could add a button that just randomly re-sizes/positions the box:
@@ -144,43 +186,41 @@ window.addEventListener("load", (e: Event) => {
 	
 	// ====== SVG element events =============
 	// You can also add event handlers to svg elements. The syntax is similar (but not identical, unfortunately) to the baseline HTML event handlers: https://svgjs.dev/docs/3.2/events/#element-on
-	box.on("click",(e : PointerEvent)=>{
-		
-	});
-
-	box.on("mousemove", (e : MouseEvent) => {
+	svg.node.addEventListener("mousedown",(e : PointerEvent)=>{
+		box.node.requestPointerLock();
 		const cursor = new Point(e.offsetX, e.offsetY);
 		if (cursor.distance_to(pivot_box.pivot1) < cursor.distance_to(pivot_box.pivot2)){
 			closer_to_pivot1 = true;
+			offsetX = pivot_box.pivot1.x;
+			offsetY = pivot_box.pivot1.y;
 		}else{
 			closer_to_pivot1 = false;
+			offsetX = pivot_box.pivot2.x;
+			offsetY = pivot_box.pivot2.y;
 		}
+
+		cursor_hori.transform({position:{x: 0, y: offsetY}, origin: "center"});
+		cursor_verti.transform({position:{x: offsetX, y: 0}, origin: "center"});
+		cursor_hori.opacity(1);
+		cursor_verti.opacity(1);
+	});
+
+	document.addEventListener("mouseup", () => {
+		document.exitPointerLock();
+		cursor_hori.opacity(0);
+		cursor_verti.opacity(0);
+	})
+
+	box.on("mousemove", (e : MouseEvent) => {
+
 	});
 
 	// ====== Dragging elements =============
 	// Because the "draggable" plugin is included, you can also set any svg element (shapee or group) to "draggable" -- this does exactly what you hope it does (i.e., it makes it so that you can click and drag the element). 
 	// Documentation here: https://github.com/svgdotjs/svg.draggable.js ... but it really is just this:
-	box.draggable();
+
 	// Of course, once the box has been dragged, you might want to know its location (e.g. in case you want to move anything else along with it). In this example, I add a new rectangle to the svg drawing:
-	let follower = svg.rect(10,10).fill("none").stroke("#ff0000");
-
 	// ...and then I update its location whenever the box is being dragged:
-	box.on("dragmove", (e)=> {
-		const handler : DragHandler = (e as CustomEvent).detail.handler;
-		const event : MouseEvent = (e as CustomEvent).detail.event;
-		e.preventDefault();
-		
-		if (closer_to_pivot1){
-			pivot_box.set_pivot1(event.offsetX, event.offsetY);
-		}else {
-			pivot_box.set_pivot2(event.offsetX, event.offsetY);
-		}
-		pivot_box.render();
-
-		let bounding = box.rbox(svg); // "rbox" is the bounding box in coordinates relative to the function's argument (in this case, the overall svg drawing area)
-		follower.size(2*canvasSize, bounding.height); // .size changes the size of the rectangle, as we saw above
-		follower.transform({position:{x:bounding.cx, y: bounding.cy}, origin: "center"}); // "cx"/"cy" are the x and y positions of the center point of the shape
-	})
 
 	// ====== Trial engine events =============
 	// The trial engine also has some events you can add handlers for:
